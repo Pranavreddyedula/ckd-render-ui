0c166bc52987229d81696bb5008da82183d845ac
Sai Pranavreddy <edulapranav@gmail.com>
2025-12-14 13:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
-import os
 
 app = Flask(__name__)
 
@@ -20,7 +18,10 @@ def index():
     if request.method == "POST":
         values = [float(request.form[f]) for f in FEATURES]
         data = scaler.transform([values])
-        pred = model.predict(data)[0]
+
+        prob = model.predict_proba(data)[0][1]
+        pred = 1 if prob >= 0.5 else 0
+        confidence = f"{round(prob*100, 2)} %"
 
         result = "NO CKD" if pred == 0 else "CKD DETECTED"
         image = "healthy_kidney.png" if pred == 0 else "ckd_kidney.png"
@@ -28,8 +29,8 @@ def index():
         return render_template(
             "result.html",
             prediction=result,
+            confidence=confidence,
             image=image,
-            confidence="High",
             causes=[
                 "Diabetes",
                 "High Blood Pressure",
@@ -48,4 +49,4 @@ def index():
     return render_template("index.html", features=FEATURES)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
